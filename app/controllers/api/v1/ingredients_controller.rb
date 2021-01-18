dd0189497fe5074a9bccfeadc7bec816f698922a
Brycen Bartolome <brycenbartolome@gmail.com>
2021-01-17 23:32:12 -0800

    working on post

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -10,4 +10,41 @@ class Api::V1::IngredientsController < ApplicationController
     render json: { status: 'SUCCESS', message: 'loaded ingredients', data: ingredients }
   end
 
+  def show
+    recipe = Recipe.find(params[:recipe_id])
+    ingredient = Ingredient.find(params[:id])
+    render json: { status: 'SUCCESS', message: 'loaded the ingredient', data: ingredient }
+  end
+
+  def create
+    recipe = Recipe.find(params[:recipe_id])
+    ingredient = recipe.ingredients.new(ingredient_params)
+    if ingredient.save
+      render json: { status: 'SUCCESS', message: 'loaded the ingredient', data: ingredient }
+    else
+      render json: { status: 'ERROR', message: 'ingredient not saved', data: ingredient.errors }
+    end
+  end
+
+  def destroy
+    ingredient = Ingredient.find(params[:id])
+    ingredient.destroy
+    render json: { status: 'SUCCESS', message: 'deleted the ingredient', data: ingredient }
+  end
+
+  def update
+    ingredient = Ingredient.find(params[:id])
+    if ingredient.update(ingredient_params)
+      render json: { status: 'SUCCESS', message: 'updated the ingredient', data: ingredient }
+    else
+      render json: { status: 'SUCCESS', message: 'loaded the ingredient', data: ingredient }
+    end
+  end
+
+  private
+
+      def ingredient_params
+        params.require(:ingredient).permit(:name)
+      end
+
 end
