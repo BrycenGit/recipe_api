49b26a469775fc0d5ed74c8a4fab2b7d39f6cbd7
Brycen Bartolome <brycenbartolome@gmail.com>
2021-01-19 17:01:17 -0800

    add /ingredients route

--- app/controllers/api/v1/ingredients_controller.rb
+++ app/controllers/api/v1/ingredients_controller.rb
@@ -4,8 +4,8 @@ class Api::V1::IngredientsController < ApplicationController
     if params[:recipe_id]
       recipe = Recipe.find(params[:recipe_id])
       ingredients = recipe.ingredients
-    # else
-    #   ingredients = Ingredient.order(created_at: :desc)
+    else
+      ingredients = Ingredient.order(created_at: :desc)
     end
     render json: { status: 'SUCCESS', message: 'loaded ingredients', data: ingredients }
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   root to: 'api/v1/recipes#index'
-  
+
   namespace 'api' do
     namespace 'v1' do
       resources :recipes do
         resources :ingredients
       end
+      resources :ingredients
     end
   end
 end
